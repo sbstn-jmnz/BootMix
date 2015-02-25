23d697ef20a948b3624249794cc9ba93e21d1703
sbstnjmnz <sbstn.jmnz@yahoo.com>
2015-02-25 01:09:29 -0300

    make it better6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'fast-everglades-4393.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "plastikilo.cl",
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: ENV["EMAIL"],
+        password: ENV["PASSWORD"]
+    }
 end
