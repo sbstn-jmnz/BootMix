2052b13ab641752011b5eb8437c46cefefb8c9be
sbstnjmnz <sbstn.jmnz@yahoo.com>
2015-02-25 19:17:18 -0300

    Mandril4

--- config/application.rb
+++ config/application.rb
@@ -21,17 +21,6 @@ module Myapp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('app', 'assets', 'audios')
-    #config.action_mailer.delivery_method = :sendmail
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.smtp_settings = {
-        address: "smtp.gmail.com",
-        port: 587,
-        domain: "plastikilo.cl",
-        authentication: "plain",
-        enable_starttls_auto: true,
-        user_name: ENV["EMAIL"],
-        password: ENV["PASSWORD"]
-    }
+    
 end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,14 +39,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { :host => 'fast-everglades-4393.herokuapp.com' }
-  config.action_mailer.smtp_settings = {
-        address: "smtp.gmail.com",
-        port: 587,
-        domain: "plastikilo.cl",
-        authentication: "plain",
-        enable_starttls_auto: true,
-        user_name: ENV["EMAIL"],
-        password: ENV["PASSWORD"]
-    }
+  
 end
