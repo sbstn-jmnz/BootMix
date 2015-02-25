4370709b81dcc506b4504d21fbd80920874e7a04
sbstnjmnz <sbstn.jmnz@yahoo.com>
2015-02-25 01:06:09 -0300

    make it better5

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-#config.action_mailer.default_url_options = { :host => 'fast-everglades-4393.herokuapp.com' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'fast-everglades-4393.herokuapp.com' }
 end
