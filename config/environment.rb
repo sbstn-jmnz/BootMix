c841f1cb7623bc321848621820e94efe299e4224
sbstnjmnz <sbstn.jmnz@yahoo.com>
2015-02-25 00:55:34 -0300

    make it better2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { :host => 'fast-everglades-4393.herokuapp.com' }
